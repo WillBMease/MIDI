8909e71266a45d0e4e0bd9f68a36e8e301c21d46
Will Mease <wmease@usc.edu>
2014-06-20 10:21:02 -0700

    d

--- testA/host.js
+++ testA/host.js
@@ -2,8 +2,8 @@ var midi = require("../midi.js");
 var count = 0 ;
 //var input = new midi.input() ;
 
-var output1 = new midi.output();
-output1.openVirtualPort("JellyVibes1");
+// var output1 = new midi.output();
+// output1.openVirtualPort("JellyVibes1");
 // var output2 = new midi.output();
 // output2.openVirtualPort("JellyVibes2");
 // setTimeout(function() {
@@ -48,42 +48,42 @@ var send = new Buffer(send0 + " " + send1 + " " + send2) ;
     if (err) throw err;
     console.log('UDP message sent to ' + HOST +':'+ PORT);
     console.log(send) ;
-    client.close();
+    //client.close();
 
  }); // end client send 
 
 });
 input.openPort(0);
 
-// server.on('listening', function () {
-//     var address = server.address();
-//     console.log('UDP Server listening on ' + address.address + ":" + address.port);
-// });
-// server.on('message', function (message, remote) {
-//     console.log(remote.address + ':' + remote.port +' - ' + message);
-//    // var d = [message[0] + " " + message[1] + " " + message[2]] ;
-//    var newMsg = message.toString('utf8');
-//    var sendMsg1 = newMsg[0] + newMsg[1] + newMsg[2] ;
-//    var sendMsg2 = newMsg[4] + newMsg[5] ;
-//    var sendMsg3 = newMsg[7] + newMsg[8] ; 
-//    var intMsg1 = parseInt(sendMsg1,10);
-//    var intMsg2 = parseInt(sendMsg2,10);
-//    var intMsg3 = parseInt(sendMsg3,10);
-//    // console.log(intMsg1) ;
-//    //    console.log(intMsg2) ;
-//    //       console.log(intMsg3) ;
-//    // console.log(sendMsg1) ;
-//    // console.log(sendMsg2) ;
-//    // console.log(sendMsg3) ;
-//     //var sendMsg = [intMsg1,intMsg2,intMsg3] ;
-//     //console.log(sendMsg) ;
-// 	output1.sendMessage([intMsg1,intMsg2,intMsg3]) ;
-// 	count += 1 ;
-// 	console.log(count) ;
-// 	//output.sendMessage([137,40,64]) ;
-//     //console.log(message) ;
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+   // var d = [message[0] + " " + message[1] + " " + message[2]] ;
+   var newMsg = message.toString('utf8');
+   var sendMsg1 = newMsg[0] + newMsg[1] + newMsg[2] ;
+   var sendMsg2 = newMsg[4] + newMsg[5] ;
+   var sendMsg3 = newMsg[7] + newMsg[8] ; 
+   var intMsg1 = parseInt(sendMsg1,10);
+   var intMsg2 = parseInt(sendMsg2,10);
+   var intMsg3 = parseInt(sendMsg3,10);
+   // console.log(intMsg1) ;
+   //    console.log(intMsg2) ;
+   //       console.log(intMsg3) ;
+   // console.log(sendMsg1) ;
+   // console.log(sendMsg2) ;
+   // console.log(sendMsg3) ;
+    //var sendMsg = [intMsg1,intMsg2,intMsg3] ;
+    //console.log(sendMsg) ;
+	output1.sendMessage([intMsg1,intMsg2,intMsg3]) ;
+	count += 1 ;
+	console.log(count) ;
+	//output.sendMessage([137,40,64]) ;
+    //console.log(message) ;
 
-// });
+});
 // //server.bind(PORT, HOST);
-// server.bind(MYPORT, MYHOST) ;
+server.bind(MYPORT, MYHOST) ;
 
