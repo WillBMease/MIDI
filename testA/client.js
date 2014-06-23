28edc161ad7915539170ba38de3bd6e8a920a3a2
Will Mease <wmease@usc.edu>
2014-06-23 10:14:10 -0700

    just host

--- testA/client.js
+++ testA/client.js
@@ -1,4 +1,4 @@
-
+// 
 
 var midi = require("../midi.js");
 
--- testA/host.js
+++ testA/host.js
@@ -16,44 +16,19 @@ var count = 0 ;
 //   output.closePort();
 // }, 20000);
 
-var PORT = 33333;
-var MYPORT = 33334 ;
-//var HOST = '10.120.91.147';
+var MYPORT = 33333 ;
+//var .... 33333
+//var HOST = '192.168.70.3';
 //var MYHOST = '10.120.79.164'
-var HOST = '10.120.79.164';
-var MYHOST = '10.120.91.147'
+var MYHOST = '206.117.88.5' ;
 //var HOST = ipAdd ;
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-var client = dgram.createSocket('udp4');
 
-var input = new midi.input();
-console.log(input.getPortCount());
-console.log(input.getPortName(0));
-input.on('message', function(deltaTime, message) {
-  //console.log('m:' + message + ' d:' + deltaTime);
 
-var send0 = message[0] ;
-var send1 = message[1] ;
-var send2 = message[2] ;
-var send = new Buffer(send0 + " " + send1 + " " + send2) ;
 
-// console.log(message[0]) ;
-// console.log(message[1]) ;
-// console.log(message[2]) ;
-
-//console.log(send) ;
-  // output.sendMessage(message) ;
-  client.send(send, 0, send.length, PORT, HOST, function(err, bytes) {
-    if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    console.log(send) ;
-    //client.close();
-
- }); // end client send 
-
-});
-input.openPort(0);
+var output1 = new midi.output();
+output1.openVirtualPort("JellyVibes");
 
 server.on('listening', function () {
     var address = server.address();
@@ -77,13 +52,57 @@ server.on('message', function (message, remote) {
    // console.log(sendMsg3) ;
     //var sendMsg = [intMsg1,intMsg2,intMsg3] ;
     //console.log(sendMsg) ;
-	output1.sendMessage([intMsg1,intMsg2,intMsg3]) ;
-	count += 1 ;
-	console.log(count) ;
-	//output.sendMessage([137,40,64]) ;
+  output1.sendMessage([intMsg1,intMsg2,90]) ;
+  count += 1 ;
+  console.log(count) ;
+  //output.sendMessage([137,40,64]) ;
     //console.log(message) ;
 
 });
 // //server.bind(PORT, HOST);
 server.bind(MYPORT, MYHOST) ;
+
+
+
+/////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+// var PORT = 33334;
+// //var ...33334
+// var HOST = '192.168.70.3';
+
+// var client1 = dgram.createSocket('udp4');
+// //var client2 = dgram.createSocket('upd4') ;
+
+
+// var input = new midi.input();
+// console.log(input.getPortCount());
+// for (var i = 0 ; i < 4 ; i++)
+// console.log(input.getPortName(i));
+// input.on('message', function(deltaTime, message) {
+//   //console.log('m:' + message + ' d:' + deltaTime);
+
+// var send0 = message[0] ;
+// var send1 = message[1] ;
+// var send2 = message[2] ;
+// var send = new Buffer(send0 + " " + send1 + " " + send2) ;
+
+// // console.log(message[0]) ;
+// // console.log(message[1]) ;
+// // console.log(message[2]) ;
+
+// //console.log(send) ;
+//   // output.sendMessage(message) ;
+//   client1.send(send, 0, send.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message sent to ' + HOST +':'+ PORT);
+//     console.log(send) ;
+//     //client.close();
+
+//  }); // end client send 
+
+// });
+// input.openPort(0);
+
+
 
