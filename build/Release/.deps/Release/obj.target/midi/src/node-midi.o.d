ad8c02c561d4292978281b1f5581fee2814fda6a
Jerry Webb <jerryweb@usc.edu>
2014-06-24 13:51:53 -0700

    Just updating the keymapping files

--- build/Release/.deps/Release/obj.target/midi/src/node-midi.o.d
+++ build/Release/.deps/Release/obj.target/midi/src/node-midi.o.d
@@ -1,28 +1,26 @@
-cmd_Release/obj.target/midi/src/node-midi.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__LINUX_ALSA__' '-DBUILDING_NODE_EXTENSION' -I/home/jerry/.node-gyp/0.10.25/src -I/home/jerry/.node-gyp/0.10.25/deps/uv/include -I/home/jerry/.node-gyp/0.10.25/deps/v8/include -I../src  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -MMD -MF ./Release/.deps/Release/obj.target/midi/src/node-midi.o.d.raw  -c -o Release/obj.target/midi/src/node-midi.o ../src/node-midi.cpp
+cmd_Release/obj.target/midi/src/node-midi.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__LINUX_ALSA__' '-DBUILDING_NODE_EXTENSION' -I/usr/include/nodejs/src -I/usr/include/nodejs/deps/uv/include -I/usr/include/nodejs/deps/v8/include -I../src  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -MMD -MF ./Release/.deps/Release/obj.target/midi/src/node-midi.o.d.raw   -c -o Release/obj.target/midi/src/node-midi.o ../src/node-midi.cpp
 Release/obj.target/midi/src/node-midi.o: ../src/node-midi.cpp \
- /home/jerry/.node-gyp/0.10.25/deps/v8/include/v8.h \
- /home/jerry/.node-gyp/0.10.25/deps/v8/include/v8stdint.h \
- /home/jerry/.node-gyp/0.10.25/src/node.h \
- /home/jerry/.node-gyp/0.10.25/deps/uv/include/uv.h \
- /home/jerry/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-unix.h \
- /home/jerry/.node-gyp/0.10.25/deps/uv/include/uv-private/ngx-queue.h \
- /home/jerry/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-linux.h \
- /home/jerry/.node-gyp/0.10.25/src/node_object_wrap.h \
- /home/jerry/.node-gyp/0.10.25/src/node.h \
- /home/jerry/.node-gyp/0.10.25/src/node_object_wrap.h \
- ../src/lib/RtMidi/RtMidi.h ../src/lib/RtMidi/RtMidi.cpp \
- ../src/lib/RtMidi/RtMidi.h
+ /usr/include/nodejs/deps/v8/include/v8.h \
+ /usr/include/nodejs/deps/v8/include/v8stdint.h \
+ /usr/include/nodejs/src/node.h /usr/include/nodejs/deps/uv/include/uv.h \
+ /usr/include/nodejs/deps/uv/include/uv-private/uv-unix.h \
+ /usr/include/nodejs/deps/uv/include/uv-private/ngx-queue.h \
+ /usr/include/nodejs/deps/uv/include/uv-private/uv-linux.h \
+ /usr/include/nodejs/src/node_object_wrap.h \
+ /usr/include/nodejs/src/node.h \
+ /usr/include/nodejs/src/node_object_wrap.h ../src/lib/RtMidi/RtMidi.h \
+ ../src/lib/RtMidi/RtMidi.cpp ../src/lib/RtMidi/RtMidi.h
 ../src/node-midi.cpp:
-/home/jerry/.node-gyp/0.10.25/deps/v8/include/v8.h:
-/home/jerry/.node-gyp/0.10.25/deps/v8/include/v8stdint.h:
-/home/jerry/.node-gyp/0.10.25/src/node.h:
-/home/jerry/.node-gyp/0.10.25/deps/uv/include/uv.h:
-/home/jerry/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-unix.h:
-/home/jerry/.node-gyp/0.10.25/deps/uv/include/uv-private/ngx-queue.h:
-/home/jerry/.node-gyp/0.10.25/deps/uv/include/uv-private/uv-linux.h:
-/home/jerry/.node-gyp/0.10.25/src/node_object_wrap.h:
-/home/jerry/.node-gyp/0.10.25/src/node.h:
-/home/jerry/.node-gyp/0.10.25/src/node_object_wrap.h:
+/usr/include/nodejs/deps/v8/include/v8.h:
+/usr/include/nodejs/deps/v8/include/v8stdint.h:
+/usr/include/nodejs/src/node.h:
+/usr/include/nodejs/deps/uv/include/uv.h:
+/usr/include/nodejs/deps/uv/include/uv-private/uv-unix.h:
+/usr/include/nodejs/deps/uv/include/uv-private/ngx-queue.h:
+/usr/include/nodejs/deps/uv/include/uv-private/uv-linux.h:
+/usr/include/nodejs/src/node_object_wrap.h:
+/usr/include/nodejs/src/node.h:
+/usr/include/nodejs/src/node_object_wrap.h:
 ../src/lib/RtMidi/RtMidi.h:
 ../src/lib/RtMidi/RtMidi.cpp:
 ../src/lib/RtMidi/RtMidi.h:
