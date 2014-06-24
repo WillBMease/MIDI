ffe47dcafd2df3a18cd5cc6fe477753b1f3ac253
Will Mease <wmease@usc.edu>
2014-06-24 15:10:43 -0700

    d

--- tcpchat/index.js
+++ tcpchat/index.js
@@ -15,18 +15,6 @@ io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg) ;
 
-			var context = new webkitAudioContext(); // Create audio container with webkit prefix
-	oscillator = context.createOscillator(); // Create bass guitar
-    gainNode = context.createGainNode(); // Create boost pedal
- 
-	oscillator.connect(gainNode); // Connect bass guitar to boost pedal
-	gainNode.connect(context.destination); // Connect boost pedal to amplifier
-	gainNode.gain.value = 0.3; // Set boost pedal to 30 percent volume
-oscillator.noteOn(0); // Play bass guitar instantly
-
-setTimeout(function(){
-oscillator.noteOff(0) ;
-}, 100)
 	});
 });
 
