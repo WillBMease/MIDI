8165d80e915328b794a2c34483657d05fc723c3a
Will Mease <wmease@usc.edu>
2014-07-14 09:34:40 -0700

    d

--- realcopy/public/js/audioTest.js
+++ realcopy/public/js/audioTest.js
@@ -27,14 +27,19 @@ function audioTest() {
 
 for (var x = 3 ; x < 32 ; x++)
 {
-	midiMsg[x] = 88
+	midiMsg[x] = x
 }
 
 for (var i = 1 ; i < 501 ; i++) {
 				midiMsg[0] = peer.label
 				midiMsg[1] = 4
 				midiMsg[2] = i
-	user[1].send(midiMsg)
+	
+	for (var y = 1 ; y < userLimit ; y++) {
+	if (user[y] != 0)
+	user[y].send(midiMsg)
+	}
+
 }
 
 }
