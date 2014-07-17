5f90d4407ef1148effceae7bb95d621fea992158
Will Mease <wmease@usc.edu>
2014-07-17 15:09:01 -0700

    d

--- realcopy/public/js/metronome.js
+++ realcopy/public/js/metronome.js
@@ -5,20 +5,22 @@ function metronome() {
 	midiMsg[1] = '2' ;
 	midiMsg[2] = 100 ;
 
-
-if (metroActive)
-	metroActive = false
-
-else if (!metroActive)
+if (!metroActive) {
 	metroActive = true
+var refreshIntervalId = setInterval(playMetronome, 1000);
+}
 
-	setInterval(function() {
-		if (metroActive) {
+else if (metroActive) {
+	metroActive = false
+	clearInterval(refreshIntervalId)
+}
+
+
+
+
+}
+
+function playMetronome() {
 		user[1].send(midiMsg)
 		triggerSample(0, midiMsg)
-	}
-
-},1000)
-
-
 }
