9e15f2814e0d94488beb864eaa3f6419f758e711
Will Mease <wmease@usc.edu>
2014-07-17 16:25:38 -0700

    d

--- realcopy/public/js/sampleMapping.js
+++ realcopy/public/js/sampleMapping.js
@@ -3,17 +3,19 @@ var noteNode = [];
 for (var i = 0 ; i < 120 ; i++)
 	noteNode[i] = 0
 
-	cabinet.connect(overdrive.input)
-	overdrive.connect(compressor.input)
-	compressor.connect(tremolo.input)
-	tremolo.connect(chorus.input)
-	chorus.connect(phaser.input)
-	phaser.connect(convolver.input)
-	convolver.connect(delay.input)
-	delay.connect(filter.input)
-	filter.connect(wahwah.input)
+for (var i = 1 ; i < userLimit ; i++) {
+	cabinet[i].connect(overdrive.input)
+	overdrive[i].connect(compressor.input)
+	compressor[i].connect(tremolo.input)
+	tremolo[i].connect(chorus.input)
+	chorus[i].connect(phaser.input)
+	phaser[i].connect(convolver.input)
+	convolver[i].connect(delay.input)
+	delay[i].connect(filter.input)
+	filter[i].connect(wahwah.input)
 
-	wahwah.connect(context.destination);
+	wahwah[i].connect(context.destination);
+}
 
 function loadInstrument(index, instr)
 {
@@ -62,17 +64,17 @@ var instrument = '<audio id="" preload="auto">' + '</audio>';
 
 	notes = noteWrap.find('audio');
 
-if (noteNode[0] != 0){
-	for (var i = 0 ; i < presetInstrument.notes ; i++)
-		noteNode[i].disconnect()
-}
+// if (noteNode[0] != 0){
+// 	for (var i = 0 ; i < presetInstrument.notes ; i++)
+// 		noteNode[i].disconnect()
+// }
 
 for (var i = 0 ; i < presetInstrument.notes ; i++)
 {
 
 	noteNode[i] = context.createMediaElementSource(notes[i])
 
-	noteNode[i].connect(cabinet.input)
+	noteNode[i].connect(cabinet[index].input)
 
 	// cabinet.connect(overdrive.input)
 	// overdrive.connect(compressor.input)
