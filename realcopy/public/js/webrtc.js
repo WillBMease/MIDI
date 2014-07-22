e2d3c24d118cdeb9854e87fd2cd3be5743909dff
Will Mease <wmease@usc.edu>
2014-07-21 17:04:27 -0700

    d

--- realcopy/public/js/webrtc.js
+++ realcopy/public/js/webrtc.js
@@ -22,6 +22,7 @@ for (var i = 0 ; i < userLimit ; i++) {
     var peer = new Peer(randID, {
             host: "54.191.34.54",
             port: 9000,
+            path: '/peerjs-server',
             config: {
               'iceServers': [
               { url: 'stun:54.186.225.6:3478?proto=udp' },
