b48019af3ee274a5e14b79b38d99209be8f7e0f4
Will Mease <wmease@usc.edu>
2014-08-06 14:07:02 -0700

    d

--- realcopy2/public/js/dataProcess.js
+++ realcopy2/public/js/dataProcess.js
@@ -45,7 +45,7 @@ var sampleCheck = []
       if (data[1] == '0' && data[2] == '0') {
         if (pingIncoming[data[0]] != data[0]){
           data[2] = '1' ;
-        for (var y = 0 ; y < 10 ; y++)
+        for (var y = 0 ; y < 1 ; y++)
         user[index].send(data) ;
         pingIncoming.push(data[0])
           }       
