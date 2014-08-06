bb252357134a9321ff7eaf9b9a85d82073925dc8
Will Mease <wmease@usc.edu>
2014-08-06 13:42:59 -0700

    d

--- realcopy2/public/js/dataProcess.js
+++ realcopy2/public/js/dataProcess.js
@@ -53,11 +53,12 @@ var sampleCheck = []
         //console.log('I received the ping: ' + data[2]);
         data[2] = '1' ;
 
-        for (var y = 0 ; y < 3 ; y++)
+        for (var y = 0 ; y < 20 ; y++)
         user[index].send(data) ;
         //console.log('Now I sent the ping back: ' + data[2])
         // $('#messages').empty().append('<br>' + 'sent ping back to ' + user[index].peer);
-}         pingIncoming.push(data[0])
+  pingIncoming.push(data[0])
+}       
       }
 
       else if (data[1] == '0' && data[2] == '1') {
