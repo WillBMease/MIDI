c0e87cef647d77a41b79d0ec75f88d30cbc269a8
Will Mease <wmease@usc.edu>
2014-08-11 14:31:55 -0700

    d

--- realcopy2/public/js/ping.js
+++ realcopy2/public/js/ping.js
@@ -24,7 +24,7 @@ function Ping(){
 
 if (!pingActive) {
       pingActive = true
-      refreshPing = setInterval(quantTest, 1000)
+      refreshPing = setInterval(pinger, 1000)
 }
 
 else if (pingActive) {
