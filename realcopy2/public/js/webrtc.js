c4cf4c85c0d3004b9c9122815d824fbb20fceed6
Will Mease <wmease@usc.edu>
2014-08-12 18:05:02 -0700

    d

--- realcopy2/public/js/webrtc.js
+++ realcopy2/public/js/webrtc.js
@@ -23,12 +23,26 @@ for (var i = 0 ; i < userLimit ; i++) {
 
 // var peer = new Peer(randID, {key: 'lwjd5qra8257b9'});
 
-    var peer = new Peer(randID, {
-            host: "54.191.34.54",
-            port: 9000,
-            path: '/peerjs-server',
-            debug: 3
-  });
+  //   var peer = new Peer(randID, {
+  //           host: "54.191.34.54",
+  //           port: 9000,
+  //           path: '/peerjs-server',
+  //           debug: 3
+  // });
+
+
+var peer = new Peer(userID, {
+          host: "54.191.34.54",
+          port: 9000,
+          path: '/peerjs-server',
+          config: {
+            'iceServers': [
+            { url: 'stun:54.186.225.6:3478' },
+            { url: 'turn:54.186.225.6:3478',
+              credential: 'jvsecretkey',
+              realm: 'jvturn'
+            }]}
+});
 
   //   var peer = new Peer(randID, {
   //           host: "54.191.34.54",
