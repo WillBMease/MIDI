e6f692543726d875a4b52d297804b11c692993a3
Will Mease <wmease@usc.edu>
2014-11-05 02:12:24 -0800

    d

--- realcopy2/public/js/webrtc.js
+++ realcopy2/public/js/webrtc.js
@@ -99,9 +99,7 @@ for (var i = 1 ; i < userLimit ; i++) {
       i = userLimit
     }
   }
-
       callPeer()
-
 }
 
 } // end connect()
@@ -132,80 +130,80 @@ for (var i = 1 ; i < userLimit ; i++) {
 
 
 
-if (!MediaStreamTrack) document.body.innerHTML = '<h1>Incompatible Browser Detected. Try <strong style="color:red;">Chrome Canary</strong> instead.</h1>';
+// if (!MediaStreamTrack) document.body.innerHTML = '<h1>Incompatible Browser Detected. Try <strong style="color:red;">Chrome Canary</strong> instead.</h1>';
 
-var experiments = document.querySelector('.experiment');
-MediaStreamTrack.getSources(function (media_sources) {
-    var sources = [];
-    for (var i = 0; i < media_sources.length; i++) {
-        sources.push(media_sources[i]);
-    }
+// var experiments = document.querySelector('.experiment');
+// MediaStreamTrack.getSources(function (media_sources) {
+//     var sources = [];
+//     for (var i = 0; i < media_sources.length; i++) {
+//         sources.push(media_sources[i]);
+//     }
 
-    getAllUserMedias(sources);
-});
+//     getAllUserMedias(sources);
+// });
 
-var devicesFetched = {};
-var index = 0;
+// var devicesFetched = {};
+// var index = 0;
 
-function getAllUserMedias(media_sources) {
-    var media_source = media_sources[index];
-    if (!media_source) return;
+// function getAllUserMedias(media_sources) {
+//     var media_source = media_sources[index];
+//     if (!media_source) return;
 
-    // to prevent duplicated devices to be fetched.
-    if (devicesFetched[media_source.id]) {
-        index++;
-        return getAllUserMedias(media_sources);
-    }
-    devicesFetched[media_source.id] = media_source;
+//     // to prevent duplicated devices to be fetched.
+//     if (devicesFetched[media_source.id]) {
+//         index++;
+//         return getAllUserMedias(media_sources);
+//     }
+//     devicesFetched[media_source.id] = media_source;
 
-    var constraints = {};
+//     var constraints = {};
 
-    if (media_source.kind == 'audio') {
-        constraints.audio = {
-            optional: [{
-                sourceId: media_source.id
-            }]
-        };
-    }
+//     if (media_source.kind == 'audio') {
+//         constraints.audio = {
+//             optional: [{
+//                 sourceId: media_source.id
+//             }]
+//         };
+//     }
 
-    if (media_source.kind == 'video') {
-        constraints.video = {
-            optional: [{
-                sourceId: media_source.id
-            }]
-        };
-    }
+//     if (media_source.kind == 'video') {
+//         constraints.video = {
+//             optional: [{
+//                 sourceId: media_source.id
+//             }]
+//         };
+//     }
 
-    navigator.webkitGetUserMedia(constraints, function (stream) {
-        experiments.appendChild(document.createElement('br'));
-        experiments.appendChild(document.createElement('br'));
+//     navigator.webkitGetUserMedia(constraints, function (stream) {
+//         experiments.appendChild(document.createElement('br'));
+//         experiments.appendChild(document.createElement('br'));
         
-        var h2 = document.createElement('div');
-        experiments.appendChild(h2);
+//         var h2 = document.createElement('div');
+//         experiments.appendChild(h2);
 
-        experiments.appendChild(document.createElement('br'));
-        experiments.appendChild(document.createElement('br'));
+//         experiments.appendChild(document.createElement('br'));
+//         experiments.appendChild(document.createElement('br'));
 
-        if (media_source.kind === 'audio') {
-            h2.innerHTML = media_source.label || 'microphone ' + index;
-        } else if (media_source.kind === 'video') {
-            h2.innerHTML = media_source.label || 'camera ' + index;
-        } else {
-            h2.innerHTML = 'Some other kind of source: ' + JSON.stringify(media_source, null, '&nbsp;');
-        }
+//         if (media_source.kind === 'audio') {
+//             h2.innerHTML = media_source.label || 'microphone ' + index;
+//         } else if (media_source.kind === 'video') {
+//             h2.innerHTML = media_source.label || 'camera ' + index;
+//         } else {
+//             h2.innerHTML = 'Some other kind of source: ' + JSON.stringify(media_source, null, '&nbsp;');
+//         }
 
-        var mediaElement = document.createElement(media_source.kind);
-        mediaElement.src = window.URL.createObjectURL(stream);
-        experiments.appendChild(mediaElement);
-        mediaElement.controls = true;
-        mediaElement.play();
-    }, function (e) {
-        experiments.appendChild(document.createElement('br'));
-        var h2 = document.createElement('div');
-        h2.innerHTML = JSON.stringify(e);
-        experiments.appendChild(h2);
-    });
+//         var mediaElement = document.createElement(media_source.kind);
+//         mediaElement.src = window.URL.createObjectURL(stream);
+//         experiments.appendChild(mediaElement);
+//         mediaElement.controls = true;
+//         mediaElement.play();
+//     }, function (e) {
+//         experiments.appendChild(document.createElement('br'));
+//         var h2 = document.createElement('div');
+//         h2.innerHTML = JSON.stringify(e);
+//         experiments.appendChild(h2);
+//     });
 
-    index++;
-    getAllUserMedias(media_sources);
-}
+//     index++;
+//     getAllUserMedias(media_sources);
+// }
